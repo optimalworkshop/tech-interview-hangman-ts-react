dd2083fc3d20ce5d8892868998b4ba8a6d70a94f
Mike Voets <mike.voets@optimalworkshop.com>
2025-01-30 22:38:13 +1300

    Migrate hangman react to Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
+import Game from './Game';
+import { faker } from '@faker-js/faker';
 
-function App() {
+export default function App() {
+  const [word, setWord] = useState(faker.word.sample({ length: { min: 5, max: 10 } }).toLowerCase());
+
+  const [gameId, setGameId] = useState(faker.string.uuid);
+
+  const playAgain = () => {
+    setWord(faker.word.sample({ length: { min: 5, max: 10 } }).toLowerCase());
+    setGameId(faker.string.uuid());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Game key={gameId} word={word} lives={10} onPlayAgainClicked={playAgain} />
     </div>
   );
 }
-
-export default App;
---
+++ src/Game.tsx
@@ -0,0 +1,78 @@
+import { useCallback, useEffect, useReducer, useState } from 'react';
+import './Game.scss';
+
+const LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+interface GameProps {
+  word: string;
+  lives: number;
+  onPlayAgainClicked: () => void;
+}
+
+export default function Game({ word, lives, onPlayAgainClicked }: GameProps) {
+  const [actualLives, setActualLives] = useState(lives);
+
+  const [guesses, addGuess] = useReducer((set: Set<string>, letter: string) => new Set<string>(set).add(letter), new Set<string>());
+
+  const masked = word
+    .split('')
+    .map(letter =>
+      guesses.has(letter) || LETTERS.indexOf(letter.toUpperCase()) === -1 ? letter : '_'
+    );
+
+  const guess = useCallback((letter: string) => {
+    if (word.indexOf(letter.toLowerCase()) === -1) {
+      setActualLives(actualLives - 1);
+    }
+    addGuess(letter.toLowerCase());
+  }, [actualLives, word]);
+
+  const gameIsWon = word
+    .split('')
+    .every(
+      letter => guesses.has(letter.toLowerCase()) || LETTERS.indexOf(letter.toUpperCase()) === -1
+    );
+
+  const gameIsLost = actualLives === 0;
+
+  useEffect(() => {
+    const keyPressed = (e: KeyboardEvent) => {
+      const letter = e.key.toUpperCase();
+      if (LETTERS.indexOf(letter) !== -1) {
+        guess(letter);
+      }
+    };
+    window.addEventListener('keypress', keyPressed);
+    return () => window.removeEventListener('keypress', keyPressed);
+  }, [guess]);
+
+  return (
+    <div className="game">
+      <h1>Hangman</h1>
+      <p className="actualLives">You have {actualLives} lives left.</p>
+      {gameIsWon && <p className="won">You won the game!</p>}
+      {gameIsLost && (
+        <p className="lost">
+          You lost the game! The word was <b>{word.toUpperCase()}</b>
+        </p>
+      )}
+      <div className="masked">{masked}</div>
+      <div className="keyboard">
+        {LETTERS.map(letter => (
+          <button
+            key={letter}
+            disabled={guesses.has(letter.toLowerCase())}
+            onClick={() => guess(letter.toLowerCase())}
+          >
+            {letter}
+          </button>
+        ))}
+      </div>
+      {(gameIsWon || gameIsLost) && (
+        <button className="play-again-button" onClick={onPlayAgainClicked}>
+          Play again
+        </button>
+      )}
+    </div>
+  );
+};
